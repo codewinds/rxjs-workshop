76c1165408dbd3397b104884e8b79fcb85fbe336
Jeff Barczewski <jeff.barczewski@gmail.com>
2018-10-16 23:28:15 -0400

    update code to rxjs@6
    typo in service

--- examples/angular/solution/src/app/api-mock.service.ts
+++ examples/angular/solution/src/app/api-mock.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Todo } from './todo';
-import { Observable } from 'rxjs/Observable';
-import 'rxjs/add/observable/of';
+import { Observable, of } from 'rxjs';
 
 @Injectable()
 export class ApiMockService {
@@ -11,25 +10,25 @@ export class ApiMockService {
   }
 
   public getAllTodos(): Observable<Todo[]> {
-    return Observable.of([
+    return of([
       new Todo({id: 1, title: 'Read article', complete: false})
     ]);
   }
 
   public createTodo(todo: Todo): Observable<Todo> {
-    return Observable.of(
+    return of(
       new Todo({id: 1, title: 'Read article', complete: false})
     );
   }
 
   public getTodoById(todoId: number): Observable<Todo> {
-    return Observable.of(
+    return of(
       new Todo({id: 1, title: 'Read article', complete: false})
     );
   }
 
   public updateTodo(todo: Todo): Observable<Todo> {
-    return Observable.of(
+    return of(
       new Todo({id: 1, title: 'Read article', complete: false})
     );
   }
--- examples/angular/solution/src/app/todo-data.service.ts
+++ examples/angular/solution/src/app/todo-data.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Todo } from './todo';
 import { ApiService } from './api.service';
-import { Observable } from 'rxjs/Observable';
+import { Observable } from 'rxjs';
 
 @Injectable()
 export class TodoDataService {
